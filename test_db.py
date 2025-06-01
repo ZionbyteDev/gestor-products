009b256295d17b66b8640a21157d5600491ba3c5
ZionByte <zionbytedev@gmail.com>
2025-06-01 13:48:39 -0400

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -2,25 +2,25 @@ from db import conectar
 from utils import producto_existe
 
 def test_producto_existe():
-    # Conectar a la base de datos
+    
     conexion = conectar()
 
     if conexion:
-        # Caso 1: Producto que existe
+        
         nombre_existente = "Laptop"
         existe = producto_existe(nombre_existente, conexion)
         print(f"¿El producto '{nombre_existente}' existe? {existe}")
 
-        # Caso 2: Producto que no existe
+        
         nombre_inexistente = "ProductoFalso"
         existe = producto_existe(nombre_inexistente, conexion)
         print(f"¿El producto '{nombre_inexistente}' existe? {existe}")
 
-        # Cerrar la conexión
+        
         conexion.close()
     else:
         print("No se pudo conectar a la base de datos.")
 
-# Ejecutar la prueba
+
 if __name__ == "__main__":
     test_producto_existe()
